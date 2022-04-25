66895a74ad7f8d1467da66bc99b1bb3ead77d374
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-04-25 20:52:32 -0300

    Video5_terminado

---
+++ Assets/Scripts/Validar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Validar : MonoBehaviour
+{
+    string contracorrecta;
+    string contrausuario;
+    public Text ingresoUsuario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contracorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarcontra()
+    {
+        contrausuario = ingresoUsuario.text;
+        if (contrausuario == contracorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
