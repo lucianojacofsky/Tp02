69940593989e46c10724ce45045af1bc90c3de0e
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-04-25 19:56:48 -0300

    Initial commit

---
+++ Assets/Scripts/Ej_01.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej_01 : MonoBehaviour
+{
+    public int edad;
+    void Start()
+    {
+        if (edad >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+}
