8e1ef870c3acc3cb81d4e72f174717bd6e6047a7
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-04-25 21:05:23 -0300

    Finished

--- Assets/Scripts/Validar.cs
+++ Assets/Scripts/Validar.cs
@@ -8,10 +8,13 @@ public class Validar : MonoBehaviour
     string contracorrecta;
     string contrausuario;
     public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelMsj;
     // Start is called before the first frame update
     void Start()
     {
         contracorrecta = "12345";
+        cartelMsj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,10 +28,14 @@ public class Validar : MonoBehaviour
         contrausuario = ingresoUsuario.text;
         if (contrausuario == contracorrecta)
         {
+            cartelMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
             Debug.Log("Bienvenido");
         }
         else
         {
+            cartelMsj.SetActive(true);
+            textoMsj.text = "Contraseña incorrecta";
             Debug.Log("Contraseña incorrecta");
         }
     }
